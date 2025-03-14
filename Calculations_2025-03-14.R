06725967e444bc8f9c218e8425105973b4d7aa56
ANSozontov <A.N.Sozontov@gmail.com>
2025-03-14 18:06:15 +0500

    code split

--- Calculations_2025-03-14.R
+++ Calculations_2025-03-14.R
@@ -1,3 +1,4 @@
+export <- FALSE
 # Data load ---------------------------------------------------------------
 library(tidyverse)
 theme_set(
@@ -7,41 +8,41 @@ theme_set(
             legend.position = "bottom"
         )
 )
-L <- c("фон_2009", "фон_2014", "буф_2009", "буф_2014", "имп_2009", "имп_2014")
+# L <- c("фон_2009", "фон_2014", "буф_2009", "буф_2014", "имп_2009", "имп_2014")
 long <- readxl::read_excel("Data/Carabidae_25.01.2023.xlsx", 
                            sheet = "main_data") %>% 
+    filter(taxa != "no_insects") %>% 
     select(-duration, -traps) %>% 
     arrange(desc(site), taxa) %>% # -taxa
     mutate(tur = factor(tur), 
-           site = fct_inorder(site),
-           zone = substr(zone, 1, 3),
-           zone4 = factor(zone, levels = c("fon", "buf", "imp", "sup")),
-           zone3 = fct_collapse(zone4, imp = c("imp", "sup")),
-           km = str_extract(site, "[:digit:]{1,}"),
-           km = as.numeric(km), 
-           year = as.factor(year), 
-           .after = "site")
+        site = fct_inorder(site),
+        zone = factor(zone, 
+            levels = c("fon", "bufer", "impact", "superimpact")),
+        zone = fct_recode(zone, 
+            "background" = "fon",
+            "industrial barren" = "superimpact"),
+        km = str_extract(site, "[:digit:]{1,}"),
+        km = as.numeric(km), 
+        year = as.factor(year), 
+        .after = "site")
 
 # 1 = turs 1 and 2 are united
-wide1 <- long %>% # div2 - turs are united
+wide <- long %>% 
     select(-num, -tur) %>% 
     pivot_wider(names_from = taxa, 
                 values_from = abu, 
                 # quantitative vartiables come from abundance (inds. per 100 trap-days)
-                values_fn = sum, values_fill = 0) %>% 
-    select(-no_insects)
+                values_fn = sum, values_fill = 0)
 
-wide1 %>% 
-    select(-km, -zone4, -zone3) %>% 
+nsp_100 <- wide %>% 
+    select(-km) %>% 
     unite("id", year, zone, site, plot, sep = "_") %>% 
-    write_delim("wide_tmp.data.csv")
-
-nsp_100 <- read_delim("wide_tmp.data.csv", show_col_types = FALSE) %>% 
     column_to_rownames("id") %>% 
     t %>% 
     as.data.frame() %>% 
     sapply(function(x){
         x <- x[x>0]
+        x <- as.numeric(as.character(x))
         if(length(x) == 0){0} else 
             if(length(x) == 1){1} else {
                 iNEXT::iNEXT(x, size = 100, se = FALSE, q = 0) %>%
@@ -50,34 +51,72 @@ nsp_100 <- read_delim("wide_tmp.data.csv", show_col_types = FALSE) %>%
                     pull(qD)
             }
     })
-file.remove("wide_tmp.data.csv")
 
 # 1 = turs 1 and 2 are united
-div1 <- tibble(wide1[,1:7], 
-               abu = apply(wide1[,8:ncol(wide1)], 1, sum),
-               nsp = apply(wide1[,8:ncol(wide1)], 1, function(a){length(a[a>0])}),
+div <- tibble(wide[,1:5], 
+               abu = apply(wide[,6:ncol(wide)], 1, sum),
+               nsp = apply(wide[,6:ncol(wide)], 1, function(a){length(a[a>0])}),
                nsp100 = nsp_100,
-               shan= vegan::diversity(wide1[,8:ncol(wide1)], 
+               shan= vegan::diversity(wide[,6:ncol(wide)], 
                                       MARGIN = 1, index = "shannon", base = exp(1))
 ) %>% 
     mutate(km2 = km^2, kmLog = log(km), .after = km) %>% 
     mutate(abuLog = log(abu+1), .after = abu)
-rm(nsp_100)
 
+rm(nsp_100)
 res <- list()
+plots <- list(NA)
+tables <- list(NA)
+
+# Rarefication ------------------------------------------------------------
+library(parallel)
+rar <- long %>% 
+    group_by(year, zone, taxa) %>% 
+    summarise(abu = sum(abu), .groups = "drop") %>% 
+    unite("id", year, zone, sep = "_") %>% 
+    split(.$id) %>% 
+    map(~sort(.x$abu[.x$abu>0])) %>% 
+    mclapply(function(a){
+        a |> 
+            as.character() |>
+            as.numeric() |>
+            iNEXT::iNEXT(size = seq(0, 100, by = 2), nboot = 99, se = TRUE, conf = 0.99) |>
+            purrr::pluck("iNextEst", "size_based") |>
+            dplyr::select(m, Method, qD, qD.LCL,   qD.UCL) 
+    } , mc.cores = getOption("mc.cores", min(c(8, parallel::detectCores())))) %>% 
+    map_df(rbind, .id = "id") %>% 
+    as_tibble() %>% 
+    separate(id, into = c("year", "zone"), sep = "_") %>% 
+    mutate(zone = factor(zone, ordered = TRUE, levels = levels(long$zone)))
+
+plots$raref <- rar %>% 
+    filter(m %in% seq(0, 100, by = 2)) %>% 
+    mutate(qD.LCL  = case_when(is.na(qD.LCL) ~ qD, TRUE ~ qD.LCL),
+           qD.UCL  = case_when(is.na(qD.UCL) ~ qD, TRUE ~ qD.UCL)) %>% 
+    ggplot(aes(x = m, y = qD, color = zone, fill = zone)) + 
+    facet_wrap(~year) +
+    geom_ribbon(aes(ymin = qD.LCL, ymax = qD.UCL), alpha = 0.2, color = NA) +
+    geom_line() + 
+    scale_color_manual(values = c("darkgreen", "#ccff00", "#ff9900", "#FF3300")) +
+    scale_fill_manual(values = c("darkgreen", "#ccff00", "#ff9900", "#FF3300")) +
+    labs(x = "Individuals", y = "Species") + 
+    theme(panel.grid = element_blank())
+
+if(!export){plots$raref}
+
 # Models template ---------------------------------------------------------
 models_fit <- function(formulas){
     fits <- tibble(ff = formulas) %>% 
         split(1:nrow(.)) %>% 
         lapply(function(a){ 
             if(str_detect(a$ff, "Segmented")){
-                #######
+                ###
                 segmented::segmented(lm(
                     str_replace_all(a$ff, "Segmented", ""), 
-                    data = div1), seg.Z = ~km)
-                #######
+                    data = div), seg.Z = ~km)
+                ###
             } else {
-                lm(formula = a$ff, data = div1)
+                lm(formula = a$ff, data = div)
             }
         }) %>% 
         tibble(ff = formulas, fit = .)
@@ -136,12 +175,12 @@ model_viz <- function(df, yy){
         geom_point(shape = 21, size = 3) + 
         facet_wrap(~year, scales = "fixed", ncol = 2) +
         theme(panel.grid = element_blank()) +
-        guides(fill="none") 
+        guides(fill="none")
 }
 
 # Abundance ---------------------------------------------------------------
 res$abundance <- c("km", "kmSegmented", "km + km2", "kmLog") %>% # "zone3", "zone4", 
-    paste0("abu ~ year + ", .) %>% 
+    paste0("abu ~ ", .) %>% 
     models_fit()
 res$abundance
 model_viz(res$abundance, "abu") + 
@@ -299,3 +338,74 @@ p <- gridExtra::grid.arrange(
 )
 
 ggsave(paste0("export/Suppl.3_", Sys.Date(), ".pdf"), plot = p, width = 8, height = 12)
+
+# Multidimensional count --------------------------------------------------
+dis <- wide %>% 
+    select(-site) %>% 
+    unite("ID", zone, year, km, plot, sep = "_") %>% 
+    column_to_rownames("ID") %>% 
+    vegan::vegdist(method = "bray", binary = FALSE)
+pc <- ape::pcoa(dis)
+eig <- pc$values$Eigenvalues
+eig <- round(eig/sum(eig)*100, 1)
+pc <- pc$vectors %>% 
+    as.data.frame() %>% 
+    rownames_to_column("ID") %>% 
+    as_tibble() %>% 
+    separate(ID, into = c("zone", "year", "site", "plot"), 
+             sep = "_") %>% 
+    mutate(zone = factor(zone, levels = levels(long$zone)))
+
+L <- expand_grid(levels(long$zone), c(2009, 2014)) %>% 
+    apply(1, function(a){paste0(a, collapse = "_")})
+
+distances <- as.matrix(dis)
+distances[upper.tri(distances)] <- NA
+diag(distances) <- NA
+distances <- distances %>%
+    as.data.frame() %>%
+    rownames_to_column("id1") %>%
+    as_tibble() %>%
+    pivot_longer(names_to = "id2", values_to = "dis", -id1) %>%
+    filter(!is.na(dis)) %>%
+    separate(id1, c("zone1", "year1"), sep = "_", extra = "drop") %>%
+    separate(id2, c("zone2", "year2"), sep = "_", extra = "drop") %>%
+    mutate(
+        id1 = factor(paste0(zone1, "_", year1), levels = L, ordered = TRUE),
+        id2 = factor(paste0(zone2, "_", year2), levels = L, ordered = TRUE),
+        .keep = "unused") %>% 
+    split(1:nrow(.)) %>% 
+    lapply(function(x){
+        if(x$id1 > x$id2) {
+            tibble(dis = x$dis, id1 = x$id2, id2 = x$id1)
+        } else {
+            tibble(dis = x$dis, id1 = x$id1, id2 = x$id2)
+        }
+    }) %>% 
+    map_dfr(rbind) %>% 
+    pivot_wider(
+        names_from = id2, values_from = dis, 
+        values_fill = NA, values_fn = mean)
+distances %>% 
+    mutate_if(is.numeric, function(a){round(a, 2)}) %>% 
+    writexl::write_xlsx(paste0("export/distances_", Sys.Date(), ".xlsx"))
+
+# Multidimensional viz ----------------------------------------------------
+ggplot(pc, aes(x = Axis.1, y = Axis.2, linetype = year,
+        fill = zone, color = zone, shape = year)) +
+    geom_point(color = "black", size = 2.5) +  
+    stat_ellipse() + 
+    scale_shape_manual(values = c(21, 22))+
+    scale_linetype_manual(values = c("dashed", "solid")) +
+    scale_fill_manual(values = c("darkgreen", "greenyellow", "orange", "red")) +
+    scale_color_manual(values = c("darkgreen", "greenyellow", "orange", "red")) +
+    labs(subtitle = "Ординация по динамической плотности\nТуры объединены", 
+         x = paste0("Ось 1 (", eig[1], " %)"), 
+         y = paste0("Ось 2 (", eig[2], " %)"), 
+         # fill = "Год", color = "Год", shape = "Год"
+    ) +
+    theme(panel.grid = element_blank())
+
+ggsave(paste0("export/Fig.3_ord_", Sys.Date(), ".svg"), width = 18, height = 13, units = "cm")
+ggsave(paste0("export/Fig.3_ord_", Sys.Date(), ".png"), width = 18, height = 13, units = "cm")
+
